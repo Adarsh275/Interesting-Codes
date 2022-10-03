70d46046812dcbe66c5469a073694adeb2f53f26
Adarsh275 <adarshaman2000@gmail.com>
2022-10-03 14:08:49 +0530

    Download file in multiple format

--- Youtube Downloads/MultipleFormat_youtube_download.py
+++ Youtube Downloads/MultipleFormat_youtube_download.py
@@ -1,20 +1,54 @@
-from ast import For
-from random import choices
 from pytube import YouTube
 
-link = input(" Enter Your URL :")
+link = input("Enter Your URL: ")
 video = YouTube(link)
+print("VIDEO TITLE: ",video.title)
 
-print(video.title)
+# List_videos = video.streams.all() # this will list all the types of formate avilable 
 
-List_videos = video.streams.all()
-# List_videos = video.streams.filter(only_audio=True) # only audio list will be displayed
-vid = list(enumerate(List_videos))
+while(1) :
+   print("\nChoose From the below Options")
+   print("1. All video file format \n2. All Only video file format \n3. All Audio file format \n4. Exit Program")
+   choose = int(input("Enter your choice : "))
 
-for i in vid:
-   print (i)
-print()
+   if choose == 1:
+      List_videos = video.streams.filter(progressive=True)
+      vid = list(enumerate(List_videos))
 
-Choice = int(input("Enter video No :"))
-List_videos[Choice].download()
-print("your video downloaded sucessfully")
+      for i in vid:
+         print(i)
+      print()
+            
+      Choice = int(input("Enter your preferred resolution No : "))
+      List_videos[Choice].download()
+      print("your video downloaded sucessfully")
+
+   elif choose == 2:
+      List_videos_Only = video.streams.filter(only_video=True)
+      vid = list(enumerate(List_videos_Only))
+
+      for i in vid:
+         print(i)
+      print()
+      
+      Choice = int(input("Enter your preferred resolution No : "))
+      List_videos_Only[Choice].download()
+      print("your video downloaded sucessfully")
+
+   elif choose == 3:
+      List_audios = video.streams.filter(only_audio=True)   
+      aud = list(enumerate(List_audios))
+
+      for i in aud:
+         print(i)
+      print()
+
+      Choice = int(input("Enter your preferred audio format No : "))
+      List_audios[Choice].download()
+      print("your audio file downloaded sucessfully")
+
+   elif choose == 4:
+      exit()
+
+   else :
+      print("Please Enter a valid Input")
